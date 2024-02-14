9a35f3c4b17182cf2a8083e20368cd1c552ff9bf
Xavi Lee <awxiaoxian2020@163.com>
2024-02-14 13:37:02 +0800

    fix baidu verification

--- packages/typescriptlang-org/gatsby-config.js
+++ packages/typescriptlang-org/gatsby-config.js
@@ -44,6 +44,7 @@ module.exports = {
         theme_color: `#3178C6`,
         display: `standalone`,
         icon: `static/icons/ts-logo-512.png`,
+        baidu_site_verification: `codeva-YrGAWpxYv8`
       },
     },
 
